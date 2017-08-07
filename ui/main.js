0989f62368d7d8025040f0149f06a875275dcf76
tmk92 <khanmohsin3011@gmail.com>
2017-08-07 19:17:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,7 @@ button.onclick= function(){
                var counter =request.responseText;
                var span =document.getElementById('count');
                span.innerHTML = counter.toString();
-                alert("hello 3");
+                
             }
         }        
     };
