3a8c3ebeb5d86476bcdb478f1ed2e80bd863c327
tmk92 <khanmohsin3011@gmail.com>
2017-08-07 21:42:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -84,6 +84,43 @@ submit.onclick= function(){
     
 };
 
+///Submit Name
+var submit =document.getElementById('submit-comment');
+submit.onclick= function(){
+    
+    //Make a request to a server and send the name
+     //Create a request to counter endpoint 
+    var request = new XMLHttpRequest();
+    
+    //Capture the res and store  it in a variable
+    request.onreadystatechange = function(){
+        
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status === 200) {
+                //Capture the list of name and render the list
+                var names = request.responseText;
+                names=JSON.parse(names);  //converted into array
+                var list='';
+                
+                for(var i=0; i< names.length; i++){
+                    list+='<li>'+names[i]+'</li>';
+                }
+                
+                var ul= document.getElementById('commentList');
+                ul.innerHTML= list;
+            }
+        }        
+    };
+     //Make a request to coounter endpoint 
+    var nameInput =document.getElementById('comment');
+    var comment= nameInput.value;
+    request.open('GET','http://khanmohsin3011.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+    
+};
+
+
 
 
 
