d902ffce7d2d5081518e7d24a5a74170cca099b0
tmk92 <khanmohsin3011@gmail.com>
2017-08-07 11:51:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,14 +17,29 @@ img.onclick=function() {
 
 //counter code
 var button= document.getElementById('counter');
-var counter= 0; 
+
 button.onclick= function(){
-    //Make a request to coounter endpoint 
+    //Create a request to counter endpoint 
+    var request = new XMLHttpRequest();
     
     //Capture the res and store  it in a variable
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status === 200){
+                var counter =request.responceText;
+                var span =document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }        
+    };
+    
+    //Make a request to coounter endpoint 
+    request.open('GET','http://khanmohsin3011.imad.hasura-app.io/counter',true);
+    request.send(null);
     
     //Render the  variable  in correct span
-     counter= counter + 1;
+     /*counter= counter + 1;
      var span =document.getElementById('count');
-     span.innerHTML = counter.toString();
+     span.innerHTML = counter.toString();*/
 };
