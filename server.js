dccead4c2673fb9ad65757ff6dcf081210c1ff9f
tmk92 <khanmohsin3011@gmail.com>
2017-08-07 21:36:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,14 +86,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-/*app.get('/:articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
     //articleName==articleOne
     //articles[articleName] ={} content object for articleOne
 
   var articleName= req.params.articleName;  //extract the name functionality provided by express    
   res.send(createTemplate(articles[articleName]));
 });
-*/
+
 
 var counter= 0;
 app.get('/counter', function (req, res) {
