00283649efdc2bdea4973bd5ebe8541fa0b8ec69
Mohsin Khan <khanmohsin3011@gmail.com>
2017-08-17 18:52:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var config={                            // configuration of db
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bodyParser.json
+app.use(bodyParser.json);
 app.use(session({
     secret: 'someRandomSecretValue',
     cookie: {maxAge :1000 *60 *60*24*30}
